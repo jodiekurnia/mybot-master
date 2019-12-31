0a0bd3df1faaf6fc10ff117d104e6c7784e1da74
Jodie Kurnia <jodiekurnia@outlook.com>
2019-12-31 23:19:18 +0700

    fixing some error

--- decenteradsBig/app/config/kill.sh
+++ decenteradsBig/app/config/kill.sh
@@ -9,6 +9,8 @@ cd /root/mybot-master/ && git pull
 #extract DB-IP
 unzip -o /root/mybot-master/dependencies/GeoLite2-City.mmdb.zip -d /root/mybot-master/decenteradsBig/app/www/inc
 unzip -o /root/mybot-master/dependencies/IP2PROXY-LITE-PX8.BIN.zip -d /root/mybot-master/decenteradsBig/app/www/inc
+#copy config nginx
+cp /root/mybot-master/decenteradsBig/app/config/default /etc/nginx/sites-available/default
 #ganti chown & chmod
 chown www-data:www-data /root/mybot-master/decenteradsBig/app/www/ -R
 chmod +x /root/mybot-master/decenteradsBig/app/www/ -R
--- install/decenteradsBig/rootdec-app.sh
+++ install/decenteradsBig/rootdec-app.sh
@@ -5,12 +5,11 @@ dpkg -i /root/mybot-master/dependencies/firefox-mozilla-build_58.0-0ubuntu1_amd6
 mkdir /root/.mozilla && mkdir /root/.mozilla/plugins && cp /root/mybot-master/dependencies/libflashplayer.so /root/.mozilla/plugins/libflashplayer.so
 #Create SymLink & set owner + chmod
 rm -rf /var/www/html
-rm -rf /etc/nginx/sites-available/default
 ln -s /root/mybot-master/decenteradsBig/app/www /var/www/html
-ls -s /root/mybot-master/decenteradsBig/app/config/default /etc/nginx/sites-available/default
 chown www-data:www-data /var/www/html
 chmod +x /var/www/html
-#update php-ini
+#copy config file php + nginx
+cp /root/mybot-master/decenteradsBig/app/config/default /etc/nginx/sites-available/default
 cp /root/mybot-master/dependencies/php.ini /etc/php/7.0/fpm/php.ini
 cp /root/mybot-master/dependencies/www.conf /etc/php/7.0/fpm/pool.d/www.conf
 #delete previous cronjob
