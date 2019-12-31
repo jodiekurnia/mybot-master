fe4b10dc59cdcad065f33622284859f8d18dc69b
Jodie Kurnia <jodiekurnia@outlook.com>
2019-12-31 20:16:04 +0700

    change how to kill kill.sh

--- decenterads/app/config/start.sh
+++ decenterads/app/config/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-killall -9 kill.sh
 for ((i=0; i<15; i++));
  do xvfb-run -a /root/mybot-master/core/slimerjs /root/mybot-master/decenterads/app/bot/decenterads.js . &
  sleep 1
 done
+killall -9 kill.sh
--- decenteradsBig/web/config/start.sh
+++ decenteradsBig/web/config/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-killall -9 kill.sh
 for ((i=0; i<75; i++));
  do xvfb-run -a /root/mybot-master/core/slimerjs /root/mybot-master/decenteradsBig/web/bot/decenteradsweb.js . &
  sleep 1
 done
+killall -9 kill.sh
