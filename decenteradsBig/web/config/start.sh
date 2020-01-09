d25fc4b64307102b70763e71a83c8cae9d07f4ff
Jodie Kurnia <jodiekurnia@outlook.com>
2020-01-09 16:38:44 +0700

    down to 75

--- decenteradsBig/web/config/start.sh
+++ decenteradsBig/web/config/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for ((i=0; i<100; i++));
+for ((i=0; i<75; i++));
  do xvfb-run -a /root/mybot-master/core/slimerjs /root/mybot-master/decenteradsBig/web/bot/decenteradsweb.js . &
  sleep 1
 done
