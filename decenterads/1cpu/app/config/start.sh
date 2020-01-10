32b456990f89ef6c35edf7cfdbfcb72e1e982c3e
Jodie Kurnia <jodiekurnia@outlook.com>
2020-01-10 15:24:08 +0700

    change 1cpu app to 30tab

--- decenterads/1cpu/app/config/start.sh
+++ decenterads/1cpu/app/config/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for ((i=0; i<25; i++));
+for ((i=0; i<30; i++));
  do xvfb-run -a /root/mybot-master/core/slimerjs /root/mybot-master/decenterads/1cpu/app/bot/decenterads.js . &
  sleep 1
 done
