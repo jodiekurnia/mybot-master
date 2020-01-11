c929885d3f261f47d76c7ede6ed71ca8de3e1c85
Jodie Kurnia <jodiekurnia@outlook.com>
2020-01-11 16:15:52 +0700

    change 225 to 175 on decenterweb 6cpu

--- decenterads/6cpu/web/config/start.sh
+++ decenterads/6cpu/web/config/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for ((i=0; i<225; i++));
+for ((i=0; i<175; i++));
  do xvfb-run -a /root/mybot-master/core/slimerjs /root/mybot-master/decenterads/6cpu/web/bot/decenteradsweb.js . &
  sleep 1
 done
