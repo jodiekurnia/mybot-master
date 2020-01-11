ce1d91815d005117e42e3c8cbe9c11a4fd748992
Jodie Kurnia <jodiekurnia@outlook.com>
2020-01-11 15:41:41 +0700

    fixing flow

--- startup/decenterads/2cpu/startup-rootdec-web.sh
+++ startup/decenterads/2cpu/startup-rootdec-web.sh
@@ -17,10 +17,8 @@ add-apt-repository "deb http://archive.canonical.com/ubuntu $(lsb_release -sc) p
 add-apt-repository multiverse
 #configure-grub
 DEBIAN_FRONTEND=noninteractive dpkg-reconfigure grub-pc
-#upgrade & install dependencies
-apt-get update -y && apt-get upgrade -y && apt-get install -y git unzip nano nginx curl wget php-fpm htop libc6 libstdc++6 libgcc1 libgtk3.0 libasound2 libxrender1 libdbus-glib-1-2 xvfb adobe-flashplugin browser-plugin-freshplayer-pepperflash php-bcmath php-curl
-#cloning repository
-git clone https://github.com/jodiekurnia/mybot-master.git /root/mybot-master
+#upgrade & install dependencies & cloning repository & hit install
+apt-get update -y && apt-get upgrade -y && apt-get install -y git unzip nano nginx curl wget php-fpm htop libc6 libstdc++6 libgcc1 libgtk3.0 libasound2 libxrender1 libdbus-glib-1-2 xvfb adobe-flashplugin browser-plugin-freshplayer-pepperflash php-bcmath php-curl && git clone https://github.com/jodiekurnia/mybot-master.git /root/mybot-master && sh /root/mybot-master/install/decenterads/2cpu/rootdec-web.sh
 #configure sshd to enable login via password
 cp /root/mybot-master/dependencies/sshd_config /etc/ssh/sshd_config
 systemctl restart sshd
@@ -28,5 +26,3 @@ systemctl restart sshd
 mkdir /etc/systemd/system/nginx.service.d
 printf "[Service]\nExecStartPost=/bin/sleep 0.1\n" > /etc/systemd/system/nginx.service.d/override.conf
 systemctl daemon-reload
-#change chmod and hit install
-sh /root/mybot-master/install/decenterads/2cpu/rootdec-web.sh
