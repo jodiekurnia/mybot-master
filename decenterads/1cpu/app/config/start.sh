eacd585a2a4c749da808d7e105b3af54b2c0a001
Jodie Kurnia <jodiekurnia@outlook.com>
2020-01-12 13:28:25 +0700

    1cpu app change tab to 15

--- decenterads/1cpu/app/config/start.sh
+++ decenterads/1cpu/app/config/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for ((i=0; i<20; i++));
+for ((i=0; i<15; i++));
  do xvfb-run -a /root/mybot-master/core/slimerjs /root/mybot-master/decenterads/1cpu/app/bot/decenterads.js . &
  sleep 1
 done
