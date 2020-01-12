63cb03597f2bc35beefb11421df9233050039f01
Jodie Kurnia <jodiekurnia@outlook.com>
2020-01-12 14:02:10 +0700

    change waiting time on start 6core

--- decenterads/6cpu/app/config/start.sh
+++ decenterads/6cpu/app/config/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 for ((i=0; i<300; i++));
  do xvfb-run -a /root/mybot-master/core/slimerjs /root/mybot-master/decenterads/6cpu/app/bot/decenterads.js . &
- sleep .1
+ sleep .5
 done
 killall -9 kill.sh
