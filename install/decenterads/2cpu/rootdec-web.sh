e97fade1bf0f7d82c96954f5f3ad48a82bf968b5
Jodie Kurnia <54063383+jodiekurnia@users.noreply.github.com>
2020-02-12 10:16:14 +0700

    add domain on decenterads+waardex web

--- install/decenterads/2cpu/rootdec-web.sh
+++ install/decenterads/2cpu/rootdec-web.sh
@@ -34,6 +34,37 @@ echo "127.0.0.1 www.fia.com" >> /etc/hosts
 echo "127.0.0.1 www.dailysportscar.com" >> /etc/hosts
 echo "127.0.0.1 automotivedigest.com" >> /etc/hosts
 echo "127.0.0.1 hondanews.com" >> /etc/hosts
+#new-add
+echo "127.0.0.1 carcar.news" >> /etc/hosts
+echo "127.0.0.1 www.time24.news" >> /etc/hosts
+echo "127.0.0.1 www.autotorque.news" >> /etc/hosts
+echo "127.0.0.1 www.montclairlocal.news" >> /etc/hosts
+echo "127.0.0.1 www.thecarconnection.com" >> /etc/hosts
+echo "127.0.0.1 mettisglobal.news" >> /etc/hosts
+echo "127.0.0.1 obor.news" >> /etc/hosts
+echo "127.0.0.1 www.motor1.com" >> /etc/hosts
+echo "127.0.0.1 www.thedrive.com" >> /etc/hosts
+echo "127.0.0.1 www.motortrend.com" >> /etc/hosts
+echo "127.0.0.1 www.autoblog.com" >> /etc/hosts
+echo "127.0.0.1 www.caranddriver.com" >> /etc/hosts
+echo "127.0.0.1 www.topgear.com" >> /etc/hosts
+echo "127.0.0.1 www.automobilemag.com" >> /etc/hosts
+echo "127.0.0.1 www.motorauthority.com" >> /etc/hosts
+echo "127.0.0.1 carbuzz.com" >> /etc/hosts
+echo "127.0.0.1 www.thetruthaboutcars.com" >> /etc/hosts
+echo "127.0.0.1 www.autoweek.com" >> /etc/hosts
+echo "127.0.0.1 www.roadandtrack.com" >> /etc/hosts
+echo "127.0.0.1 www.just-auto.com" >> /etc/hosts
+echo "127.0.0.1 www.rushlane.com" >> /etc/hosts
+echo "127.0.0.1 driving.ca" >> /etc/hosts
+echo "127.0.0.1 www.topspeed.com" >> /etc/hosts
+echo "127.0.0.1 www.nextgreencar.com" >> /etc/hosts
+echo "127.0.0.1 www.automotiveworld.com" >> /etc/hosts
+echo "127.0.0.1 www.autospies.com" >> /etc/hosts
+echo "127.0.0.1 practicalmotoring.com.au" >> /etc/hosts
+echo "127.0.0.1 www.drive.com.au" >> /etc/hosts
+echo "127.0.0.1 www.whichcar.com.au" >> /etc/hosts
+
 #enable php-fpm and nginx
 systemctl enable nginx && systemctl enable php7.0-fpm
 #set chmod
