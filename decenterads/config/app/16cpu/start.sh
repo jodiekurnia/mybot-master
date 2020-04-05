52486a57332deac309235128a68b775d78227a14
Jodie Kurnia <54063383+jodiekurnia@users.noreply.github.com>
2020-04-06 03:19:15 +0700

    change 16core to 225tabs

--- decenterads/config/app/16cpu/start.sh
+++ decenterads/config/app/16cpu/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for ((i=0; i<175; i++));
+for ((i=0; i<225; i++));
  do xvfb-run -a /root/mybot-master/core/slimerjs /root/mybot-master/decenterads/bot/app/decenterads.js . &
  sleep .2
 done
--- decenterads/config/web/16cpu/start.sh
+++ decenterads/config/web/16cpu/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for ((i=0; i<175; i++));
+for ((i=0; i<225; i++));
  do xvfb-run -a /root/mybot-master/core/slimerjs /root/mybot-master/decenterads/bot/web/decenteradsweb.js . &
  sleep .2
 done
